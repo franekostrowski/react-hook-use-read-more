d0513303f2b7109048bc42818b811bf292fa419f
franciszekostrowski <franekostrowski@protonmail.com>
2022-07-13 16:22:17 +0200

    feat: add example usage

--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,26 @@
-import 'react-app-polyfill/ie11';
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import * as ReactDOM from "react-dom"
+import useReadMore from "../src/index"
 
 const App = () => {
+  const { firstPart, secondPart
+  } = useReadMore("Hello world foo bar", { treshold: 75, firstPartMaxLength: 2 })
+
+  const [showMore, setShowMore
+  ] = React.useState<boolean>(false)
+
+  const handleClick = () => {
+    setShowMore(!showMore)
+  }
+
   return (
-    <div>
-      <Thing />
-    </div>
+    <p>
+      {firstPart}
+
+      <button onClick={handleClick}>Show more</button>
+
+      {showMore ? secondPart : ""}
+    </p>
   );
 };
 
