67226e6c9e7fbecd9d3911557cae314c2cf90e2f
franciszekostrowski <franekostrowski@protonmail.com>
2022-07-13 23:54:06 +0200

    feat: rewrite tests

---
+++ test/UseReadMore.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import { Config } from "../src/types"
+
+import useReadMore from "../src/index"
+
+interface UseReadMoreTestComponentProps {
+  text: string
+  config?: Config
+}
+
+const UseReadMoreTestComponent: React.FC<UseReadMoreTestComponentProps> = ({ text, config }) => {
+
+  if (!config) config = {};
+
+  const { firstPart, secondPart } = useReadMore(text, config)
+
+  return (
+    <>
+      <p data-testid="first-part">First part: {firstPart}</p>
+      <p data-testid="second-part">Second part: {secondPart}</p>
+    </>
+  )
+}
+
+export default UseReadMoreTestComponent
--- test/useReadMore.test.tsx
+++ test/useReadMore.test.tsx
@@ -1,46 +1,27 @@
-import {
-  renderHook,
-  RenderHookResult,
-  Renderer,
-} from '@testing-library/react-hooks';
-import useReadMore from '../src/index';
+import { render, screen } from "@testing-library/react"
+import "@testing-library/jest-dom"
+import UseReadMore from "./UseReadMore"
 
-let renderedHook: RenderHookResult<
-  unknown,
-  { firstPart: string; secondPart: string },
-  Renderer<unknown>
->;
+it("should split 'Hello world foo bar' text into firstPart (Hello world) and secondPart (foo bar)", () => {
+  render(<UseReadMore text="Hello world foo bar" />)
+  expect(screen.getByTestId("first-part")).toHaveTextContent("Hello world")
+  expect(screen.getByTestId("second-part")).toHaveTextContent("foo bar")
+});
 
-describe('useReadMore', () => {
-  it("should split 'Hello world foo bar' text into firstPart (Hello world) and secondPart (foo bar)", () => {
-    renderedHook = renderHook(() => useReadMore('Hello world foo bar'));
-    expect(renderedHook.result.current.firstPart).toEqual('Hello world');
-    expect(renderedHook.result.current.secondPart).toEqual('foo bar');
-  });
+it("should split 'Hello world foo bar' text at 75% treshold into firstPart (Hello world foo) and secondPart (bar)", () => {
+  render(<UseReadMore text="Hello world foo bar" config={{ treshold: 75 }} />)
+  expect(screen.getByTestId("first-part")).toHaveTextContent("Hello world foo")
+  expect(screen.getByTestId("second-part")).toHaveTextContent("bar")
+});
 
-  it("should split 'Hello world foo bar' text at 75% treshold into firstPart (Hello world foo) and secondPart (bar)", () => {
-    renderedHook = renderHook(() =>
-      useReadMore('Hello world foo bar', { treshold: 75 })
-    );
-    expect(renderedHook.result.current.firstPart).toEqual('Hello world foo');
-    expect(renderedHook.result.current.secondPart).toEqual('bar');
-  });
+it("should split 'Banana apple pear peach strawberry raspberry' text with firstPart's max length set for 2 into firstPart (Banana apple) and secondPart (pear peach strawberry)", () => {
+  render(<UseReadMore text="Banana apple pear peach strawberry raspberry" config={{ firstPartMaxLength: 2 }} />)
+  expect(screen.getByTestId("first-part")).toHaveTextContent("Banana apple")
+  expect(screen.getByTestId("second-part")).toHaveTextContent("pear peach strawberry raspberry")
+});
 
-  it("should split 'Banana apple pear peach strawberry raspberry' text with firstPart's max length set for 2 into firstPart (Banana apple) and secondPart (pear peach strawberry)", () => {
-    renderedHook = renderHook(() =>
-      useReadMore('Banana apple pear peach strawberry raspberry', {
-        firstPartMaxLength: 2,
-      })
-    );
-    expect(renderedHook.result.current.firstPart).toEqual('Banana apple');
-    expect(renderedHook.result.current.secondPart).toEqual(
-      'pear peach strawberry raspberry'
-    );
-  });
-
-  it("should split 'Hello world foo' text into firstPart (Hello) and secondPart (world foo)", () => {
-    renderedHook = renderHook(() => useReadMore('Hello world foo'));
-    expect(renderedHook.result.current.firstPart).toEqual('Hello');
-    expect(renderedHook.result.current.secondPart).toEqual('world foo');
-  });
+it("should split 'Hello world foo' text into firstPart (Hello) and secondPart (world foo)", () => {
+  render(<UseReadMore text="Hello world foo" />)
+  expect(screen.getByTestId("first-part")).toHaveTextContent("Hello")
+  expect(screen.getByTestId("second-part")).toHaveTextContent("world foo")
 });
